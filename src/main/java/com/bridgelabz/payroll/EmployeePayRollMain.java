15fcf3d9775d9d0a7012c54207987cfc89934bd8
RajendraGund <rajendragund2015@gmail.com>
2021-09-27 07:50:50 +0530

    Added : Welcome message for the User

---
+++ src/main/java/com/bridgelabz/payroll/EmployeePayRollMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.payroll;
+
+public class EmployeePayRollMain {
+    public static void main(String[] args) {
+        System.out.println("Welcolme to employee payRoll");
+    }
+
+}
